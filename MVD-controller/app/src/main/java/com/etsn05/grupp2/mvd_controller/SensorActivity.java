a17b31dae2162775c297f68558af15853c65b962
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-09-26 11:16:17 +0200

    Added textfield to sensor view and started working on backend coms.

--- MVD-controller/app/src/main/java/com/etsn05/grupp2/mvd_controller/SensorActivity.java
+++ MVD-controller/app/src/main/java/com/etsn05/grupp2/mvd_controller/SensorActivity.java
@@ -10,4 +10,7 @@ public class SensorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
     }
+    protected void getAll(){
+
+    }
 }

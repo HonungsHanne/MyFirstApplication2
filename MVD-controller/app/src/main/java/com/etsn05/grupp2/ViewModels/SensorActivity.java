f729e6c290f254bb2d246a1d23700ca754e296d2
andersbuhl <anders@localhost.localdomain>
2016-09-26 16:24:22 +0200

    Added special text to title of sensor

--- MVD-controller/app/src/main/java/com/etsn05/grupp2/ViewModels/SensorActivity.java
+++ MVD-controller/app/src/main/java/com/etsn05/grupp2/ViewModels/SensorActivity.java
@@ -2,6 +2,7 @@ package com.etsn05.grupp2.ViewModels;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SensorActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class SensorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+        TextView t = (TextView)findViewById(R.id.textView14);
+        t.setText("Mickes bajs");
     }
     protected void getAll(){
 

f9c9220493978b145cd432921b8ab28c19d64ec4
patrik <pat.danielsson@gmail.com>
2016-09-26 16:58:36 +0200

    Added skeleton for methods in SensorActivity.java

--- MVD-controller/app/src/main/java/com/etsn05/grupp2/ViewModels/SensorActivity.java
+++ MVD-controller/app/src/main/java/com/etsn05/grupp2/ViewModels/SensorActivity.java
@@ -5,15 +5,44 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SensorActivity extends AppCompatActivity {
-
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
-        TextView t = (TextView)findViewById(R.id.textView14);
+        this.t = (TextView) findViewById(R.id.textView14);
         t.setText("Mickes bajs");
     }
-    protected void getAll(){
+
+    protected void getAll() {
+        t.setText("ITS WORKING -- anikin always");
+    }
+
+    protected void getTemperature() {
+
+    }
+
+    protected void getPressure() {
+
+    }
+
+    protected void getHumidity() {
+
+    }
+
+    protected void getMagnet() {
+
+    }
+
+    protected void getGyroscope() {
+
+    }
+
+    protected void getAccelerometer() {
+
+    }
+
+    protected void clearAll() {
 
     }
 }

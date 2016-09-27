bd3ee4fed64eabc5cb1dabab88227795400ed6f8
HonungsHanne <hanneheingard@hotmail.com>
2016-09-27 15:26:29 +0200

    lite kod

--- MVD-controller/app/src/main/java/com/etsn05/grupp2/ViewModels/LightBulbActivity.java
+++ MVD-controller/app/src/main/java/com/etsn05/grupp2/ViewModels/LightBulbActivity.java
@@ -24,16 +24,24 @@ public class LightBulbActivity extends AppCompatActivity {
 
 
 
+
     }
 
 
 
     public void onClickGet(View view) {
-        lightBulb.blue="TO BE PARSED FROM JSON";
-        lightBulb.red="TO BE PARSED FROM JSON";
-        lightBulb.green="TO BE PARSED FROM JSON";
-        lightBulb.white="TO BE PARSED FROM JSON";
-        lightBulb.color="#"+lightBulb.red+lightBulb.green+lightBulb.blue+lightBulb.white;
+        ch.updateColor(lightBulb);
+        String color = lightBulb.color;
+        String [] RGB = new String [3];
+        RGB[0] = color.substring(0,2); //Hämtar röd
+        RGB[1] = color.substring(2,4); //hämtar grön
+        RGB[2] = color.substring(4,6); //hämtar blå
+        RGB[3] = color.substring(6,8); //hämtar vit
+        lightBulb.red = RGB[0];
+        lightBulb.green = RGB[1];
+        lightBulb.blue = RGB[2];
+        lightBulb.white = RGB[3];
+
         TextView textRed = (TextView) findViewById(R.id.textRed);
         TextView textGreen = (TextView) findViewById(R.id.textGreen);
         TextView textBlue = (TextView) findViewById(R.id.textBlue);
@@ -51,11 +59,11 @@ public class LightBulbActivity extends AppCompatActivity {
         TextView textGreen = (TextView) findViewById(R.id.textGreen);
         TextView textBlue = (TextView) findViewById(R.id.textBlue);
         TextView textWhite = (TextView) findViewById(R.id.textWhite);
-        lightBulb.red=textRed.getText().toString();
-        lightBulb.green=textGreen.getText().toString();
-        lightBulb.blue=textBlue.getText().toString();
-        lightBulb.white=textWhite.getText().toString();
-        lightBulb.color="#"+lightBulb.red+lightBulb.green+lightBulb.blue+lightBulb.white;
+        lightBulb.red = textRed.getText().toString();
+        lightBulb.green = textGreen.getText().toString();
+        lightBulb.blue = textBlue.getText().toString();
+        lightBulb.white = textWhite.getText().toString();
+        lightBulb.color =lightBulb.red+lightBulb.green+lightBulb.blue+lightBulb.white;
 
 
         // SEND COLORS
